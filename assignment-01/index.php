fdf45a66c422152001fae748fed1b755ca9fd635
mwemig02 <mwemig02@seattlecentral.edu>
2014-04-14 05:28:09 -0700

    Test

--- assignment-01/index.php
+++ assignment-01/index.php
@@ -1,3 +1,16 @@
 <!doctype html>
 <html>
-Jello World!
+  <head>
+    <title>ITC240 Assignment 01</title>
+  </head>
+  <body>
+    <h1>Assignment 01 test html!!!</h1>
+	
+	<?php
+		include("head.inc");//function to include code from another file
+		include("body.inc");
+		include("footer.inc");
+	?>
+	
+  </body>
+</html>
