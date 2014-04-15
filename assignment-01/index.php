56c3ec6836b13d5e4c82b07301d243128c605288
mwemig02 <mwemig02@seattlecentral.edu>
2014-04-14 19:39:05 -0700

    index4assignment01

--- assignment-01/index.php
+++ assignment-01/index.php
@@ -1,16 +1,13 @@
-<!doctype html>
-<html>
-  <head>
-    <title>ITC240 Assignment 01</title>
-  </head>
-  <body>
-    <h1>Assignment 01 test html!!!</h1>
-	
-	<?php
-		include("head.inc");//function to include code from another file
-		include("body.inc");
-		include("footer.inc");
-	?>
-	
-  </body>
-</html>
+<?php
+	$index_title = "ITC240 Assignment 01";
+	//PHP functions to include source code from other files:
+	include("head.inc");
+	include("body.inc");
+	include("footer.inc");
+
+	/* I did find if you introduce a PHP var before the include that uses that var an error is introduced.
+	 * To place the $index_title after an include which uses the var and error occurs and is
+	 * displayed in the title of the page!!!  Since that is where I echoed the var.
+	 * There is no magic wand to replace the procedural nature of programming.
+	*/
+?>
