73d037aa3c9716243f0588224c08f4e7c1a834bc
mwemig02 <mwemig02@seattlecentral.edu>
2014-05-20 18:43:19 -0700

    Table is just starting to work

--- assignment-05/index.php
+++ assignment-05/index.php
@@ -5,43 +5,7 @@ $thisTitle = "ITC240 Assignment 05";
 $thisH1 = "Willy Loman Expenses";
 include("head.inc");
 
-$newEntry = false;
-$pressedUpdate = false;
-
-include("password.php");
-
-//connect to database's table:
-$connectTBL = new mysqli("localhost", "mwemig02", $Secret, "mwemig02");
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-	$insertQuery = 'INSERT INTO logExpense (Month, Day, Year, expenseItem, expenseValue) VALUES (?, ?, ?, ?, ?);';
-  
-	$scrubedQuery = $connectTBL->prepare($insertQuery);
-	
-	// Reflect the column values in the table:
-	$scrubedQuery->bind_param("iiiss", $_REQUEST["Month"], $_REQUEST["Day"], $_REQUEST["Year"], $_REQUEST["expenseItem"], $_REQUEST["expenseValue"]);
-
-	$scrubedQuery->execute();
-	
-	// This version is cleaned up from all the chaos of commenting out lines and lingering variables
-	
-	$returnedData = $scrubedQuery->get_result();
-
-	echo "<pre>";
-		foreach ($returnedData as $presentData) {
-			print_r($presentData);
-		}
-
-
- 
- 
-}
-
-
 ?>
-
 
 <form action="index.php" method="POST">
 	
@@ -80,6 +44,58 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	<input name="expenseValue" placeholder="$: Expense Value">
 	<input type="Submit" value="Submit Entry">
 </form>
+
+<?php
+
+include("password.php");
+
+//connect to database's table: connectTBL
+$connectTBL = new mysqli("localhost", "mwemig02", $Secret, "mwemig02");
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+/*	THIS works and focusing on presenting 
+
+	$insertQuery = 'INSERT INTO logExpense (Month, Day, Year, expenseItem, expenseValue) VALUES (?, ?, ?, ?, ?);';
+  
+	$scrubedQuery = $connectTBL->prepare($insertQuery);
+	
+	// Reflect the column values in the table:
+	$scrubedQuery->bind_param("iiiss", $_REQUEST["Month"], $_REQUEST["Day"], $_REQUEST["Year"], $_REQUEST["expenseItem"], $_REQUEST["expenseValue"]);
+
+	$scrubedQuery->execute();
+
+	// This version is cleaned up from all the chaos of commenting out lines and lingering variables
+
+*/
+
+	$queryTBL = 'SELECT * FROM logExpense';
+	
+	$selectData = $connectTBL->prepare($queryTBL);	
+
+	$selectData->execute();
+
+	$returnedData = $selectData->get_result();
+
+//Ahh, I'm in my other class and time is running low !!! I actually understand from your e-mailed explanation that an insert wouldn't have a "get_result()" return 
+	
+?>
+<table>
+<?php	
+	foreach ($returnedData as $presentData) {
+	//print_r($presentData["expenseItem"]);
+	//Table, just started to code and test
+?>
+		<tr>
+		<td><?= $presentData["expenseItem"] ?>
+<?php
+	}// END Foreach
+?>
+</table>
+<?php	
+} // END IF Posted	
+?>
 
 <?php
 include("footer.inc");
