a71985465d537c4eeaf4df1d8d4279ad8a83127d
mwemig02 <mwemig02@seattlecentral.edu>
2014-06-03 06:55:16 -0700

    Use of the head file

---
+++ assignment-07/incHead.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+
+
+	<link rel="stylesheet" type="text/css" href="../incCSS/inc.css">
+
+	<title>
+			<?= $thisTitle ?>
+	</title>
+</head>
+	<body>
+		<center>
+			<h1><?= $thisH1 ?></h1>
+		<center>
