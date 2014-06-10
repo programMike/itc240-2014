911a7a711087e614eceb9241d880e60eebc9866f
mwemig02 <mwemig02@seattlecentral.edu>
2014-06-10 06:37:15 -0700

    head vars needed to be include directly

---
+++ assignment-08/head.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<?php
+
+// The variables became undefined in the index.php???, this was the only way to fix for now
+$thisTitle = "ITC240 Assignment 08 - functions";
+$thisH1 = "Neko's - \"Pounds Away\"";
+?>
+
+	<link rel="stylesheet" type="text/css" href="../incCSS/inc.css">
+
+	<title>
+			<?= $thisTitle ?>
+	</title>
+</head>
+	<body>
+		<center>
+			<h1><?= $thisH1 ?></h1>
+		<center>
